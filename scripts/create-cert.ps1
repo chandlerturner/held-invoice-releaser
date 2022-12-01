b82b739626337bba1e2d19d32dd770c25ef018f3
Chandler Turner <development@chandlerturner.com>
2022-12-01 15:02:16 -0600

    Add Scripts to Solution

--- scripts/create-cert.ps1
+++ scripts/create-cert.ps1
@@ -4,10 +4,11 @@ $certPassword = Read-Host -Prompt 'Create password for certificate'
 $outputDirectory = "$env:TEMP\certificate"
 $outputPath = "$outputDirectory\$dnsName.pfx"
 
-Write-Host "Creating certificate at $outputPath"
-New-Item $outputDirectory -ItemType Directory -ea 0
-
 $cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname $dnsName
 $password = ConvertTo-SecureString -String $certPassword -Force -AsPlainText
 $path = $certStoreLocation + $cert.thumbprint
+Write-Host "Creating certificate at $outputPath"
+New-Item $outputDirectory -ItemType Directory -ea 0
 Export-PfxCertificate -cert $path -FilePath $outputPath -Password $password
+
+Invoke-Item $outputDirectory
